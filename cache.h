853705d229db1d1c1d5bba6c079e298c874d0492
Tome Nelson <tomenelson@gmail.com>
2017-12-09 00:15:32 +0200

    including full test for insertion and LRU standing tests

--- cache.h
+++ cache.h
@@ -11,27 +11,25 @@ typedef enum{
 } EVICTION;
 
 class Memory{
-private:
+// private:
+public:
   int memorySize;
   int block_size;
   char* data;
 
   char randomChar(){
     srand(time(NULL));
-    int i = rand() % ('z' - 'a');
-    if( 'a' + i < 'a' || 'a' + i > 'z'){
-      throw randomExecption();
-    }
-    return 'a' + i;
+    int i = rand() % 256;
+    return i;
   }
 
 public:
   class randomExecption : std::exception {};
 
   Memory(int size, int start_val, int block_size){
-    memorySize = size;
-    block_size = block_size;
-    data = new char[memorySize];
+    this->memorySize = size;
+    this->block_size = block_size;
+    this->data = new char[memorySize];
     for(int i = 0; i < memorySize; i++){
       if(start_val == 0){
         *(data + i) = 0xff;
@@ -42,9 +40,19 @@ public:
     }
   }
 
+  ~Memory(){
+    delete[] data;
+  }
+
   char* getBlock(int address){
     char* new_block = new char[block_size];
-    int offset = address % block_size;
+    int offset;
+    if(address == 0){
+	offset = 0;
+    }
+    else{
+	offset = address % block_size;
+    }
     for(int i = 0; i < block_size; i++){
       *(new_block + i) = *(data + address - offset + i);
     }
@@ -60,8 +68,9 @@ public:
 
 //class that is one entry in the cache including metadata
 class cacheEntry{
-private:
-  int LRUstanding; //most recentry used = 0, least recently used = set_size - 1
+// private:
+public:
+  int LRUstanding; //most recently used = 0, least recently used = set_size - 1
   int valid;
   char* data;
   int tag;
@@ -79,6 +88,7 @@ public:
 
   //make a new cache entry of size block_size that is empty
   cacheEntry(int block_size){
+    this->block_size = block_size;
     LRUstanding = -1;
     valid = -1;
     tag = -1;
@@ -86,7 +96,7 @@ public:
   }
 
   ~cacheEntry(){
-    delete data;
+    delete[] data;
   }
 
   //get word from entry at a certian offset
@@ -131,12 +141,17 @@ public:
     LRUstanding = new_LRU;
   }
 
+  void updateTag(int new_tag){
+    tag = new_tag;
+  }
+
 };
 
 
 //this class is a set of cacheEntries in the cache
 class set{
-private:
+// private:
+public:
   int blocks_per_set;
   cacheEntry** data;
   EVICTION policy;
@@ -147,10 +162,10 @@ public:
   class randomExecption : std::exception {};
   //creats a new set with a specific amount of blocks of a specific size.
   set(int blocks_per_set, int block_size, EVICTION evic){
-    blocks_per_set = blocks_per_set;
-    policy = evic;
-    data = new cacheEntry*[blocks_per_set];
-    for ( int i = 0; i < blocks_per_set; i++){
+    this->blocks_per_set = blocks_per_set;
+    this->policy = evic;
+    this->data = new cacheEntry*[blocks_per_set];
+    for ( int i = 0; i < this->blocks_per_set; i++){
       cacheEntry* temp = new cacheEntry(block_size);
       *(data + i) = temp;
     }
@@ -160,7 +175,7 @@ public:
     for( int i = 0; i < blocks_per_set; i++){
       delete *(data + i);
     }
-    delete data;
+    delete[] data;
   }
 
   // find a valid entry with this tag in the set, if none exists - returns NULL
@@ -174,22 +189,27 @@ public:
   }
   // updates the LRU standing inside the block so that the entry with this tag is the most recently used.
   void updateLRU(int tag){
-    int prev_LRU;
+    int prev_LRU = -2;
     for(int i = 0; i < blocks_per_set; i++){
       if((*(data + i))->getTag() == tag){
         prev_LRU = (*(data + i))->getLRU();
       }
     }
     for( int i = 0; i < blocks_per_set; i++){
-      if((*(data + i))->getLRU() <= prev_LRU){
-        int temp = (*(data + i))->getLRU();
-        if(temp == prev_LRU){
-          (*(data + i))->updateEntryLRU(0);
-        }
-        else{
-          (*(data + i))->updateEntryLRU(temp + 1);
-        }
-      }
+	if((*(data + i))->getTag() == tag){
+	  (*(data + i))->updateEntryLRU(0);
+	}
+	else{
+	  if( (*(data + i))->getLRU() == -1){
+		continue;
+	  }
+	  else{
+		if((*(data + i))->getLRU() <= prev_LRU){
+			int temp = (*(data + i))->getLRU();
+			(*(data + i))->updateEntryLRU(temp + 1);
+		}
+	  }
+	}
     }
   }
 
@@ -204,23 +224,29 @@ public:
 
   int findEvicRandom(){
     std::srand(time(NULL));
-    int i = std::rand() % (blocks_per_set - 1);
-    if( i < 0 || i >= blocks_per_set){
-      throw randomExecption();
-    }
+    int i = std::rand() % blocks_per_set;
     return i; // FIXME how to randomize this??
   }
 
-  void updateBlock(char* new_data){
-    int i;
-    if(policy == LRU){
-      i = findEvicLRU();
+  void updateBlock(char* new_data, int tag){
+    int i = -1;
+    for( int j = 0; j < blocks_per_set; j++){
+	if((*(data + j))->getValid() != 1){
+		i = j;
+		break;
+	}
     }
-    else{
-      i = findEvicRandom();
+    if( i == -1){
+	 if(policy == LRU){
+     		 i = findEvicLRU();
+    	}
+    	else{
+      	i = findEvicRandom();
+    	}
     }
     cacheEntry* entry = *(data + i);
     entry->updateEntryData(new_data);
+    entry->updateTag(tag);
     entry->updateValid(1);
   }
 
@@ -248,19 +274,19 @@ public:
 
   // create a new cache
   Cache(int cache_size, int block_size, int associativity, int evic){
-    cache_size = cache_size;
-    block_size = block_size;
-    num_of_blocks = cache_size / block_size;
-    num_of_sets = associativity;
-    blocks_per_set = num_of_blocks / num_of_sets;
+    this->cache_size = cache_size;
+    this->block_size = block_size;
+    this->num_of_blocks = cache_size / block_size;
+    this->blocks_per_set = associativity;
+    this->num_of_sets = num_of_blocks / blocks_per_set;
     if( evic == 0 ){
-      policy = RANDOM;
+      this->policy = RANDOM;
     }
     else{
-      policy = LRU;
+      this->policy = LRU;
     }
 
-    data = new set*[num_of_sets];
+    this->data = new set*[num_of_sets];
     for (int i = 0; i < num_of_sets; i++){
       set* temp = new set(blocks_per_set, block_size, policy);
       *(data + i) = temp;
@@ -276,11 +302,11 @@ public:
     for(int i = 0; i < num_of_sets; i++){
       delete *(data + i);
     }
-    delete data;
+    delete[] data;
   }
 
   // should be invoked when servicing a load word command. Will update laod hit\miss stats as well as update the cache if it is a miss returns a single char that is the answer to the load request.
-  char loadWord(int address, Memory mem){
+  char loadWord(int address, Memory& mem){
     int bits_index_and_offset = log(block_size) + log(num_of_sets);
     int index = address % (int)(pow(2, bits_index_and_offset));
     index = index / block_size;
@@ -298,17 +324,17 @@ public:
     else{
       load_misses++;
       char* new_block = mem.getBlock(address);
-      current->updateBlock(new_block);
+      current->updateBlock(new_block, tag);
       current->updateLRU(tag);
       char result = *(new_block + offset);
-      delete new_block;
+      delete[] new_block;
       return result;
     }
   }
 
 
   // should be invoked when servicing a store word command
-  void storeWord(int address, Memory mem, char word){
+  void storeWord(int address, Memory& mem, char word){
     int bits_index_and_offset = log(block_size) + log(num_of_sets);
     int index = address % (int)(pow(2, bits_index_and_offset));
     index = index / block_size;
@@ -319,6 +345,7 @@ public:
     cacheEntry* entry = current->findEntryTag(tag);
     if(entry != NULL){
       store_hits++;
+      mem.replaceWord(address, word);
       entry->updateEntryData(offset, word);
       return;
     }
@@ -326,9 +353,9 @@ public:
       store_misses++;
       mem.replaceWord(address, word);
       char* new_block = mem.getBlock(address);
-      current->updateBlock(new_block);
+      current->updateBlock(new_block, tag);
       current->updateLRU(tag);
-      delete new_block;
+      delete[] new_block;
       return;
     }
 
--- cache_test.cpp
+++ cache_test.cpp
@@ -2,37 +2,151 @@
 #include "cache.h"
 
 
-bool allocationTest(){
+void allocationTest(){
+  std::cout << "starting allocation test...";
   Cache cache(4,1,4,1);
   if(cache.getLoadHits() != 0){
-    return false;
+    std::cout << "FAILED" << std::endl;
+    return;
   }
   if(cache.getLoadMisses() != 0){
-    return false;
+    std::cout << "FAILED" << std::endl;
+    return;
   }
   if(cache.getStoreHits() != 0){
-    return false;
+    std::cout << "FAILED" << std::endl;
+    return;
   }
   if(cache.getStoreMisses() != 0){
-    return false;
+    std::cout << "FAILED" << std::endl;
+    return;
   }
 
   if(cache.num_of_blocks != 4){
-    return false;
+    std::cout << "FAILED" << std::endl;
+    return;
   }
-  if(cache.blocks_per_set != 1){
-    return false;
+  if(cache.blocks_per_set != 4){
+    std::cout << "FAILED" << std::endl;
+    return;
   }
   if(cache.policy != LRU){
-    return false;
+    std::cout << "FAILED" << std::endl;
+    return;
   }
-
-  return true;
+  std::cout << "PASSED" << std::endl;
+  return;
 }
 
-int main(){
-  if(!allocationTest()){
-    std::cout << "failed allocation test :(";
+void memAllocationTest(){
+ Memory mem(16, 0, 1);
+}
+
+void storeWordTest(){
+  std::cout << "starting the store word test...";
+  Cache cache(8,2,2,1);
+  Memory mem(32, 0, 2);
+  cache.storeWord(0, mem, 'a');
+  if(*(mem.data) != 'a'){
+    std::cout << "FAILED" << std::endl;
+    return;
   }
+  set* currentSet = *(cache.data);
+  cacheEntry* entry = *(currentSet->data);
+  if(*(entry->data) != 'a'){
+    std::cout << "FAILED" << std::endl;
+    return;
+  }
+  if(entry->getValid() != 1){
+	std::cout << "FAILED (entry not set to valid)" << std::endl;
+	return;
+  }
+  if(entry->getLRU() != 0){
+	std::cout << "FAILED (entry LRU not correct valid)" << std::endl;
+	return;
+  }
+  if(cache.load_hits != 0 || cache.load_misses != 0 || cache.store_hits != 0 || cache.store_misses != 1){
+    std::cout << "FAILED (first store, cache miss)" << std::endl;
+    return;
+  }
+
+  cache.storeWord(0, mem, 'a');
+  if(*(mem.data) != 'a'){
+    std::cout << "FAILED" << std::endl;
+    return;
+  }
+  currentSet = *(cache.data);
+  entry = *(currentSet->data);
+  if(*(entry->data) != 'a'){
+    std::cout << "FAILED" << std::endl;
+    return;
+  }
+  if(cache.load_hits != 0 || cache.load_misses != 0 || cache.store_hits != 1 || cache.store_misses != 1){
+    std::cout << "FAILED (second store, cache hit)" << std::endl;
+    return;
+  } 
+
+  cache.storeWord(5, mem, 'b');
+  if(cache.load_hits != 0 || cache.load_misses != 0 || cache.store_hits != 1 || cache.store_misses != 2){
+    std::cout << "FAILED (third store, cache miss)" << std::endl;
+    return;
+  }
+
+  cache.storeWord(5, mem, 'c');
+  if(cache.load_hits != 0 || cache.load_misses != 0 || cache.store_hits != 2 || cache.store_misses != 2){
+    std::cout << "FAILED (fourth store, cache hit)" << std::endl;
+    return;
+  }
+
+  cache.storeWord(10, mem, 'd');
+  if(cache.load_hits != 0 || cache.load_misses != 0 || cache.store_hits != 2 || cache.store_misses != 3){
+    std::cout << "FAILED (fifth store, cache miss)" << std::endl;
+    return;
+  }
+
+  cache.storeWord(0, mem, 'd');
+  if(cache.load_hits != 0 || cache.load_misses != 0 || cache.store_hits != 2 || cache.store_misses != 4){
+    std::cout << "FAILED (sixth store, cache miss)" << std::endl;
+    return;
+  }
+
+  if( cache.loadWord(0, mem) != 'd'){
+	std::cout << "FAILED (first load, data hit)" << std::endl;
+	return;
+  }
+  if(cache.load_hits != 1 || cache.load_misses != 0 || cache.store_hits != 2 || cache.store_misses != 4){
+    std::cout << "FAILED (first load, cache hit)" << std::endl;
+    return;
+  }
+
+  
+  if ( cache.loadWord(5, mem) != 'c'){
+	std::cout << "FAILED (second load, data miss " << cache.loadWord(5, mem) << ")" << std::endl;
+	return;
+  }
+ if(cache.load_hits != 1 || cache.load_misses != 1 || cache.store_hits != 2 || cache.store_misses != 4){
+    std::cout << "FAILED (second load, cache miss)" << std::endl;
+    return;
+  }
+
+if( cache.loadWord(10, mem) != 'd'){
+	std::cout << "FAILED (third load, data miss)" << std::endl;
+	return;
+}
+  if(cache.load_hits != 1 || cache.load_misses != 2 || cache.store_hits != 2 || cache.store_misses != 4){
+    std::cout << "FAILED (third store, cache miss)" << std::endl;
+    return;
+  }
+
+  std::cout << "PASSED" << std::endl;
+  return;
+}
+
+
+
+int main(){
+  allocationTest();
+memAllocationTest(); 
+ storeWordTest();
   return 0;
 }
